5e07f981dfdf33ac5a76a8f2da09d8a4f00b020f
Vlad <141331688+SpaceOC@users.noreply.github.com>
2024-04-11 13:24:50 +0700

    Update CORE_info.h

--- source/Core/CORE_info.h
+++ source/Core/CORE_info.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define CORE_VERSION            "0.1 Alpha"
+#define CORE_VERSION            "0.2 Alpha"
 #define CORE_NAME               "NRC"
 #define CORE_DEVELOPER          "SpaceOC"
